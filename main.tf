1a7174e4ac37e1fc9e5497eac53f5d6eaf1c9d0e
Omer Koren <omer@chaseapp.io>
2019-09-01 18:26:18 +0300

    invalidator

---
+++ main.tf
@@ -0,0 +1,60 @@
+data "aws_s3_bucket" "this" {
+  bucket = "${var.bucket_name}"
+}
+
+
+resource "aws_s3_bucket_notification" "this" {
+  bucket = "${data.aws_s3_bucket.this.id}"
+
+ lambda_function {
+    lambda_function_arn = "${aws_lambda_function.this.arn}"
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+#Lambda
+locals {
+  base_path = "${path.module}/src"
+}
+
+data "local_file" "invalidatorpy" {
+  filename = "${local.base_path}/invalidator.py"
+}
+
+resource "local_file" "invalidatorpy" {
+  content = "${data.local_file.invalidatorpy.content}"
+  filename = "${local.base_path}/.archive/invalidator.py"
+}
+
+data "local_file" "initpy" {
+  filename = "${local.base_path}/__init__.py"
+}
+
+resource "local_file" "initpy" {
+  content = "${data.local_file.initpy.content}"
+  filename = "${local.base_path}/.archive/__init__.py"
+}
+
+data "archive_file" "this" {
+  depends_on = [
+    "local_file.initpy",
+    "local_file.invalidatorpy"
+  ]
+
+  type = "zip"
+  output_path = "${local.base_path}/.archive.zip"
+  source_dir = "${local.base_path}/.archive"
+}
+
+resource "aws_lambda_function" "this" {
+
+  filename = "${data.archive_file.this.output_path}"
+  source_code_hash = "${data.archive_file.this.output_base64sha256}"
+
+  function_name    = "${var.name}"
+  runtime = "python3.7"
+  role    = "${aws_iam_role.this.arn}"
+  memory_size = 128
+  timeout = 60
+  handler = "invalidator.lambda_handler"
+}
---
+++ role.tf
@@ -0,0 +1,55 @@
+resource "aws_lambda_permission" "this" {
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.this.arn}"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${data.aws_s3_bucket.this.arn}"
+}
+
+
+resource "aws_iam_role" "this" {
+  name = "${var.name}-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "1",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "this" {
+  name = "${var.name}-policy"
+  path = "/"
+
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:*",
+        "s3:*",
+        "cloudfront:*"
+      ],
+      "Resource": "*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role = "${aws_iam_role.this.name}"
+  policy_arn = "${aws_iam_policy.this.arn}"
+}
